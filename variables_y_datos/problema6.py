a92daae67200fcf0471c0b5807d0ee019f1300b5
02Ajay20 <52421393+02Ajay20@users.noreply.github.com>
2022-09-09 12:01:07 -0500

    prove

--- variables_y_datos/problema6.py
+++
@@ -1,8 +0,0 @@
-#Saca el valor del 16% IVA de un valor especifico
-
-print("escribe un valor y obten el iva (16%) de ese valor")
-value = float(input("Escribe un valor en pesos colombianos: "))
-iva = value * .16
-total = value + iva
-
-print(f"\nEl 16% de iva de {value} es {iva}\nEl valor mas el 16% de iva el {total}")
